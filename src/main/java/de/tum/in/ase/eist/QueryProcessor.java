dbb8fbce361fa415a3d7e8bf2e2296c9515d80f5
ge54noj <felix.handoko@tum.de>
2022-07-14 14:43:05 +0200

    W

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "AB";
+           return "12";
         } else { // TODO extend the programm here
             return "Who is she?";
         }
