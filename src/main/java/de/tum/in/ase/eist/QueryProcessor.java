927c4c3616f69575495a0700a7bd721006257820
ge54noj <felix.handoko@tum.de>
2022-07-14 14:49:06 +0200

    W

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,7 +14,7 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
            return "AB";
         } else { // TODO extend the programm here
-            return "Who is she?";
+            return "";
         }
     }
 }
